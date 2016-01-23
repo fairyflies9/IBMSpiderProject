d342ea30487d45ede0832fbded8006f37acb1ecb
Laura Miller <lam9@unc.edu>
2016-01-22 19:09:49 -0500

    Lower domain
    Larger domain so that spider has larger distance for free fall

--- generate_mesh2d.m
+++ generate_mesh2d.m
@@ -2,8 +2,8 @@
 %This file will generate a rectangular "leaf" that resists bending and stretching. 
 %It will be held in place by a line of target points along its center
 
-L = 0.3;                              % length of computational domain (m)
-N = 512;                            % number of Cartesian grid meshwidths at the finest level of the AMR grid
+L = 0.6;                              % length of computational domain (m)
+N = 1024;                            % number of Cartesian grid meshwidths at the finest level of the AMR grid
 dx = L/N;                           % Cartesian mesh width (m)
 
 drag_length = 0.10;                  % drag line length (m)
@@ -12,7 +12,8 @@ npts = npts_drag;                   % total number points on each dragline
 ds = drag_length/(npts_drag-1);     % drag line spacing (m)
 
 mesh_name = 'spider2d_';            % structure name
-offset = 0.0;	                    % plate offset from center of domain (m)
+xoffset = 0.0;	                    % plate offset from center of domain (m)
+yoffset = 0.15;
 
 kappa_spring = 2.0e1;               % spring constant (Newton)
 kappa_beam = 5.0e-15;                % beam stiffness constant (Newton m)
@@ -28,8 +29,8 @@ vertex_fid = fopen([mesh_name num2str(N) '.vertex'], 'w');
 fprintf(vertex_fid, '%d\n', npts);
 
 for s = 0:npts_drag-1
-   X(1) = offset;
-   X(2) = 0.0 + 0.5*drag_length  - s*ds;
+   X(1) = xoffset;
+   X(2) = yoffset + 0.5*drag_length  - s*ds;
    fprintf(vertex_fid, '%1.16e %1.16e\n', X(1), X(2));
 end 
 
